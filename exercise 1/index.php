70ecbd20ae6089eb059267656cf432a10c5d4be3
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-06-26 10:19:56 +0200

    First exercise

---
+++ exercise 1/index.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+class Beverage {
+    private $color;
+    private $price;
+    private $temperature;
+
+    public function __construct($color, $price, $temperature = "cold")
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo() {
+        return "This beverage is " . $this->temperature . " , " . $this->color . " and cost " . $this->price . "€" . ".<br>";
+    }
+
+    public function getTemperature() {
+        return "The temperature is " . $this->temperature . ".";
+    }
+}
+
+$beverage = new Beverage("black", 2.00);
+$info = $beverage->getInfo();
+echo $info;
+
+$temp = $beverage->getTemperature();
+echo $temp;
+?>
